d73b2a53121c1e22c156a74c29977c490e037f89
AbrahamDN <abe.ndinga@gmail.com>
2023-08-02 10:47:54 +0100

    refactor: customize home page, style Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,38 @@
-import { CardProps } from "./Card.types.ts";
+import "./Card.styles.css";
 
-const Card = ({ room }: CardProps) => {
+import Markup from "../Markup/Markup.tsx";
+import { CardProps } from "./Card.types.ts";
+import { Hotel } from "lucide-react";
+
+const Card = ({
+  room: { name, description, images, type, facilities },
+}: CardProps) => {
   return (
-    <div className="room-card">
-      <h3 className="h5">{room.name}</h3>
-      <p>{room.description}</p>
-      {/* Add other room information here */}
-    </div>
+    <article className="card">
+      <div className="card__image">
+        <img
+          src={images[0].filename}
+          alt={images[0].alt}
+          className="card__img"
+        />
+      </div>
+
+      <h3 className="h5 card__name">{name}</h3>
+
+      <Markup className="card__desc" markup={description} />
+
+      <div className="card__type">
+        <Hotel /> <span>{type.name}</span>
+      </div>
+
+      <ul className="card__facilities">
+        {facilities.slice(0, 3).map((facility) => (
+          <li key={facility.id} className="facility">
+            {facility.label}
+          </li>
+        ))}
+      </ul>
+    </article>
   );
 };
 
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,3 +1,4 @@
+import "./Home.styles.css";
 import { Link } from "react-router-dom";
 import Card from "../../components/card/Card";
 import { accommodations } from "../../data/accommodation.json";
@@ -6,14 +7,23 @@ import Layout from "../../layout/Layout";
 const HomePage = () => {
   return (
     <Layout>
-      <main className="container">
-        <h1>Accommodation Booking Website</h1>
+      <main className="page container">
+        <div className="page-header">
+          <h1 className="h4 title">Your Australia Accommodation</h1>
+          <p className="standfirst">
+            Discover Your Perfect Stay: Explore a Diverse Range of Australian
+            Accommodations - from Stunning Coastal Resorts to Charming Outback
+            Retreats. Find Your Ideal Escape and Uncover the Beauty of Australia
+          </p>
+        </div>
 
-        {accommodations.map((room) => (
-          <Link key={room.id} to={`/room/${room.id}`}>
-            <Card room={room} />
-          </Link>
-        ))}
+        <section className="accoms">
+          {accommodations.map((room) => (
+            <Link key={room.id} to={`/room/${room.id}`}>
+              <Card room={room} />
+            </Link>
+          ))}
+        </section>
       </main>
     </Layout>
   );
