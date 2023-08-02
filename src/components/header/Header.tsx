91928e095889d9c295ebbd98a4e084cc1c633d00
AbrahamDN <abe.ndinga@gmail.com>
2023-08-02 09:53:04 +0100

    refactor: update meta data

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import Search from "../search/Search";
 import "./Header.styles.css";
 
 const Header = () => {
   return (
     <header className="container header">
-      <div className="header-logo">KabHotel</div>
+      <div className="header-logo">
+        <Link to="/">KaBooking</Link>
+      </div>
 
       <div className="header-search">
         <Search />
